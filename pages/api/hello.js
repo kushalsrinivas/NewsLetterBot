7019f03ab83d9b2e71edea3d6547b6dd8ebf8645
kushalsrinivas <kushal.s.2005@gmail.com>
2023-09-01 15:59:43 +0530

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,29 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+const mailer = require("nodemailer");
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const sender = "newsletterbot379@gmail.com";
+  var transporter = mailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: sender,
+      pass: "liippiqozztjjojb",
+    },
+  });
+
+  var mailOptions = {
+    from: sender,
+    to: "kushal.s.2005@gmail.com",
+    subject: "Sending Email using Node.js",
+    text: "That was easy!",
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.status(200).json({ error: error });
+    } else {
+      console.log("Email sent: " + info.response);
+      res.status(200).json({ name: info.response });
+    }
+  });
+  console.log("waauwa");
 }
