80f86d3da45492ce3231f8ac33209ad16b6cf777
kushalsrinivas <kushal.s.2005@gmail.com>
2023-09-17 18:25:49 +0530

    upadeted src

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <body>
-      <div className="container">
+    <div style={{ background: "white" }}>
+      <div style="container">
         <h1>Promptly Launched</h1>
+        <h2>The Most waited chat gpt plugin is here</h2>
+        <button>Download Now</button>
+        <button>Git Hub </button>
+        <button>Product Hunt </button>
       </div>
-    </body>
+    </div>
   );
 }
